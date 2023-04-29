a76e3fb821997baae5f8744aa785311fb54dbee6
Yousef Bigdeli <yousefbigdeli96@gmail.com>
2023-04-29 13:48:16 +0430

    Add signin route

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,54 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { connectDatabase, getDocument } from "@/lib/db";
+import { verifyPassword } from "@/lib/auth";
+
+export default NextAuth({
+  session: {
+    jwt: true,
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      async authorize(credentials) {
+        let client, user;
+
+        try {
+          client = await connectDatabase();
+        } catch (error) {
+          console.log(error.message);
+          throw new Error("Authentication faliled.");
+        }
+
+        try {
+          user = await getDocument(client, "users", {
+            email: credentials.email,
+          });
+        } catch (error) {
+          console.log(error.message);
+          client.close();
+          throw new Error("Authentication faliled.");
+        }
+
+        if (user.length === 0) {
+          client.close();
+          throw new Error("No user found!");
+        }
+
+        const isValid = await verifyPassword(
+          credentials.password,
+          user[0].password
+        );
+
+        if (!isValid) {
+          client.close();
+          throw new Error("Password is not correct.");
+        }
+
+        client.close();
+        return { email: user[0].email };
+      },
+    }),
+  ],
+});
