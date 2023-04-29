04a22bb33c654a1ec4fd1360b5a61d94fab44243
Yousef Bigdeli <yousefbigdeli96@gmail.com>
2023-04-29 14:00:44 +0430

    Add react-toastify

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import { SessionProvider } from "next-auth/react";
-
+import { ToastContainer } from "react-toastify";
 import Layout from "@/components/layout/layout";
+
 import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,6 +11,7 @@ function MyApp({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <ToastContainer />
     </SessionProvider>
   );
 }
