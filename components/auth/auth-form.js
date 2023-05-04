d70bba2af28ba6534c837cb4dbbfec9dbe77eb35
Yousef Bigdeli <yousefbigdeli96@gmail.com>
2023-05-04 20:27:41 +0430

    Add login and signup

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,33 +1,107 @@
-import { useState } from 'react';
-import classes from './auth-form.module.css';
+import { useState, useRef } from "react";
+import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
+import { toast } from "react-toastify";
+import classes from "./auth-form.module.css";
+
+const toastOptions = {
+  position: "top-right",
+  autoClose: 4000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "light",
+};
+
+async function createUser(email, password) {
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Somthing went wrong!");
+  }
+
+  return data;
+}
 
 function AuthForm() {
+  const router = useRouter();
   const [isLogin, setIsLogin] = useState(true);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
   }
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const email = emailInputRef.current.value;
+    const password = passwordInputRef.current.value;
+
+    // TODO: add validation
+
+    if (isLogin) {
+      const result = await signIn("credentials", {
+        redirect: false,
+        email,
+        password,
+      });
+
+      if (!result.ok) {
+        toast.error(result.error, toastOptions);
+      } else {
+        toast.success("Successfully signed in.", toastOptions);
+        emailInputRef.current.value = "";
+        passwordInputRef.current.value = "";
+        router.replace("/profile");
+      }
+    } else {
+      try {
+        const result = await createUser(email, password);
+        console.log(result);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  };
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" ref={emailInputRef} required />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            ref={passwordInputRef}
+            required
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
