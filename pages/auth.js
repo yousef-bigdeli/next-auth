165cd7ee189bc0942d438a5db2fbff2127e78269
Yousef Bigdeli <yousefbigdeli96@gmail.com>
2023-05-04 20:11:11 +0430

    Redirect after login

--- pages/auth.js
+++ pages/auth.js
@@ -1,6 +1,24 @@
 import AuthForm from "@/components/auth/auth-form";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+
+  if (isLoading) return <p style={{ textAlign: "center" }}>loading...</p>;
+
   return <AuthForm />;
 }
 
